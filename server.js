21d0d0f4df65f624ed509caa70349b34fd9b288a
Jonah Foster <jonahfoster97@gmail.com>
2022-03-28 10:46:31 -0500

    created get and post routes in server.js

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const path = require('path');
 const indexRoutes = require('./routes/index');
 const notesRoutes = require('./routes/notesRoutes')
 
+app.use(express.json());
 app.use(express.urlencoded({
     extended: true
 }));
@@ -18,3 +19,42 @@ app.use(express.static('public'));
 app.listen(PORT, () => {
     console.log(`API server port: ${PORT}!`);
 }); 
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  });
+  
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'notes.html'));
+  });
+  
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Create a get route for 
+app.get('/api/notes', (req, res) => {
+    fs.readFile('db/db.json', 'utf8', function(err, contents) {
+      var notes = JSON.parse(contents);
+      res.send(notes);
+    });
+  });
+
+
+//   Create a POST route for new notes
+app.post('/api/notes', (req, res) => {
+    fs.readFile('db/db.json',(err, data) => {
+      if (err) throw err;
+      let json = JSON.parse(data);
+      let note = {
+        title: req.body.title,
+        text: req.body.text,
+        id: uuidv1()
+      }
+      json.push(note);
+  
+      fs.writeFile('db/db.json', JSON.stringify(json, null, 2), (err) => {
+        if (err) throw err;
+        res.send('200');
+      });
+    });
+  });
+
